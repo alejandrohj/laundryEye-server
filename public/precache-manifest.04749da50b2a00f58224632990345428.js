406c670e8f1cf522cfda8c2374f2b49bf8a2ce45
Alejandro Hernández <alejandro_hj.11@outlook.com>
2020-09-19 21:10:02 +0100

    predeploy-client

---
+++ public/precache-manifest.04749da50b2a00f58224632990345428.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6104fa564919e17a09c534600d9577f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65dee87bb32cbf1d5f95",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "668d6d7a6af1a7b4e6ef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65dee87bb32cbf1d5f95",
+    "url": "/static/js/2.c597ccaf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c597ccaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668d6d7a6af1a7b4e6ef",
+    "url": "/static/js/main.1201322a.chunk.js"
+  },
+  {
+    "revision": "8ff6177ea688166a7432",
+    "url": "/static/js/runtime-main.fae96dc2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
