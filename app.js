dd600153bb8b7524f2a7c89024bbdcd2cb34e62a
Alejandro Hernández <alejandro_hj.11@outlook.com>
2020-09-21 12:50:04 +0100

    deploy

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(cors({
 
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
