e0f11b99803270a52446cf9e4b2dacdcdcf9b179
Alejandro Hernández <alejandro_hj.11@outlook.com>
2020-10-29 17:18:42 +0000

    Carts washer deploy to SCADA

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.880ee24ab4e9a21fc6112bd217032a61.js"
+  "/precache-manifest.00eccdb8c27502ff16783d3e5576b980.js"
 );
 
 self.addEventListener('message', (event) => {
