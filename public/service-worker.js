bec05001524b4874e59d9970b3b673addd503a2c
Alejandro Hernández <alejandro_hj.11@outlook.com>
2021-01-13 09:10:00 +0000

    smaller smart phones warehouse table style fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8828a60eae88a9ef2b2704f795dcdf49.js"
+  "/precache-manifest.02a28183906fbd4660df1343d7401095.js"
 );
 
 self.addEventListener('message', (event) => {
