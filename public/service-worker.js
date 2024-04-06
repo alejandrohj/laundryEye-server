6e1c7f755d1126208d6d92d5acb6a75f9723e096
Alejandro Hernández Jorge <alejandro_hj.11@outlook.com>
2024-04-06 12:09:17 +0100

    implemented offset cnº3 , and salobre and gregory added to lavan calendar

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57595963ab17109ea7cbb3c250df7a98.js"
+  "/precache-manifest.1cc769fb5660fbf0e4d8204fd9b52190.js"
 );
 
 self.addEventListener('message', (event) => {
